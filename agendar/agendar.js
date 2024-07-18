85140bc7fae09bd42edb8f3f43ff188a433fe1e4
Alan <depzz123@gmail.com>
2024-07-18 16:06:57 -0300

    ATUALIZAÇAO

---
+++ agendar/agendar.js
@@ -0,0 +1,5 @@
+const page1 = document.getElementById('img-header')
+
+page1.addEventListener('click', function(){
+    location.href = '../page1.html'
+})
--- serviços/servicos.js
+++ serviços/servicos.js
@@ -1,3 +1,33 @@
 function redirectTo(url) {
     window.location.href = url;
 }
+
+const page1 = document.getElementById('img-header')
+
+page1.addEventListener('click', function(){
+    location.href = '../../page1.html'
+})
+
+function redirectTo(url) {
+    window.location.href = url;
+}
+
+
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
+
