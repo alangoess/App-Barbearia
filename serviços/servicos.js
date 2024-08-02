85422792947446cf7c2ad241a952983c6c1639be
Alan Gabriel <depzz123@gmail.com>
2024-08-01 22:08:58 -0300

    att

--- serviços/servicos.js
+++ serviços/servicos.js
@@ -1,14 +1,12 @@
 function redirectTo(url) {
     window.location.href = url;
 }
-/*Menu*/
+
+
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
-<<<<<<< HEAD
-let imgHeader = document.getElementById('img-header');
-=======
->>>>>>> 5df2d8352d8d2ceb95df5f6fbedb9097af596da8
+
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
@@ -21,13 +19,4 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
-/*Menu*/
-
-<<<<<<< HEAD
-imgHeader.addEventListener('click', ()=>{
-    location.href = '../page1.html'
-})
-=======
->>>>>>> 5df2d8352d8d2ceb95df5f6fbedb9097af596da8
-
 
